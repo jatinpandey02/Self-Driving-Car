dddd838a05946d4aae048c835940c9aeea6fa009
jatinpandey02 <34341002+jatinpandey02@users.noreply.github.com>
2020-08-25 19:18:36 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,18 @@
-from tensorflow.keras.layers import Conv2D,Flatten, Dense, Dropout, BatchNormalization, LeakyReLU, MaxPool2D
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.models import Sequential
-from tensorflow.keras import regularizers
-from tensorflow.keras.optimizers import Adam
-import cv2
-from tensorflow.keras.preprocessing import image
 import numpy as np
+from keras.preprocessing import image
+from keras.layers import Conv2D,Flatten, Dense, Dropout, BatchNormalization, MaxPool2D
+from keras.optimizers import Adam
+from keras.models import Sequential
+from keras import regularizers
+from keras.optimizers import Adam
+import scipy.misc
+# import model
+import cv2
+import os
+
+adam = Adam(0.0001)
+
+L2NormConst = 0.001
 
 model = Sequential()
 
@@ -41,32 +48,32 @@ model.add(Dense(1,activation='tanh',kernel_regularizer=regularizers.l2(0.01)))
 
 model.summary()
 
-model.summary()
-model.load_weights("sdc2.h5")
+model.compile(optimizer = Adam(0.0001),loss = 'mse',metrics=['mae'])
+
+model.load_weights("gkl7.h5")
 
 img_str = cv2.imread('steering_wheel_image copy.jpg',0)
 rows,cols = img_str.shape
 
 smoothed_angle = 0
 
-f = open("07012018/data.txt")
-labels = f.readlines()
-f.close()
-
-i = 60000
-while( cv2.waitKey(1) & 0xFF != ord('q') and i<70000):
-    img = cv2.imread("07012018/data/"+str(i)+".jpg")
-    cv2.imshow("frame", cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
-    img = cv2.resize(img,(200,200))
-    img = np.array(img)/255.0
-    
-    img = np.reshape(img,[1,200,200,3])
-    degrees = model.predict(img)*180/np.pi
-    smoothed_angle += 0.2 * pow(abs((degrees - smoothed_angle)), 2.0 / 3.0) *(degrees - smoothed_angle) / abs(degrees - smoothed_angle)
-    M = cv2.getRotationMatrix2D((cols/2,rows/2),-smoothed_angle,1)
-    dst = cv2.warpAffine(img_str,M,(cols,rows))
-
-    cv2.imshow("steering wheel", dst)
+i = 36000
+while(cv2.waitKey(10) != ord('q') and i<44000):
+    file = os.path.exists("driving_dataset/" + str(i) + ".jpg")
+    if(file):
+        img1 = image.load_img("driving_dataset/" + str(i) + ".jpg",color_mode='rgb')
+        img1 = image.img_to_array(img1)/255.0
+        img = image.load_img("driving_dataset/" + str(i) + ".jpg",color_mode='rgb',target_size=[200,200])
+        img = image.img_to_array(img)/255.0
+        img_resh = np.reshape(img,[1,200,200,3])
+        degrees = float(model.predict(img_resh) * 180.0 / scipy.pi)
+        print("Predicted steering angle: " + str(degrees) + " degrees")
+        cv2.imshow("frame", cv2.cvtColor(img1, cv2.COLOR_RGB2BGR))
+        smoothed_angle += 0.2 * pow(abs((degrees - smoothed_angle)), 2.0 / 3.0) * (degrees - smoothed_angle) / abs(degrees - smoothed_angle)
+        #rotated = imutils.rotate_bound(img_str, degrees)
+        M = cv2.getRotationMatrix2D((cols/2,rows/2),-smoothed_angle,1)
+        dst = cv2.warpAffine(img_str,M,(cols,rows))
+        cv2.imshow("steering wheel", dst)
     i += 1
 
 cv2.destroyAllWindows()
